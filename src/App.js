4bb9da3b97a79beb1ef416e55e5bb85bf6927f1d
spandanakandera <skandera@1800flowers.com>
2020-08-10 19:04:12 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import './App.scss';
 import Layout from './containers/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src='' className="App-logo" alt="logo" />
-      </header>
+      <header className="App-header" />
         <div className="container">
           <Layout/>
         </div>
